af56536a10813ef59cb3ec8a1883c77aced91d08
joao.reis <joao.reis@telefonicati.onmicrosoft.com>
2026-07-30 16:47:24 -0300

    feat: update logging configuration to support environment variables for sensitive fields and mask settings

--- src/main/java/br/com/tlf/shared/constants/ApplicationConstants.java
+++ src/main/java/br/com/tlf/shared/constants/ApplicationConstants.java
@@ -6,7 +6,6 @@ public class ApplicationConstants {
 
     public static final String APPLICATION_NAME = "CREDIT_CORE_CONSENT_BMS";
     public static final String CLASS_METHOD_MESSAGE_PATTERN = "Class {}, method {}";
-    public static final int DEFAULT_CODECS_SIZE = 16 * 1024 * 1024;
     public static final Long MAX_VALIDITY_DAYS = 30L;
     public static final String AGGREGATE_TYPE = "ConsentRegisteredEvent";
     public static final String TOPIC_NAME = "vivopay.credit.consent.events.v1";
