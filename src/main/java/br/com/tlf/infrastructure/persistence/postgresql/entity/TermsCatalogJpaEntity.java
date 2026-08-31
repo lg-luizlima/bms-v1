880de8928f69db063b8d19c5e7c41b879a1f47c8
joao.reis <joao.reis@telefonicati.onmicrosoft.com>
2026-08-31 17:16:42 -0300

    feat:rebase migrations to exclude old database architecture

--- src/main/java/br/com/tlf/infrastructure/persistence/postgresql/entity/TermsCatalogJpaEntity.java
+++ src/main/java/br/com/tlf/infrastructure/persistence/postgresql/entity/TermsCatalogJpaEntity.java
@@ -1,19 +1,21 @@
 package br.com.tlf.infrastructure.persistence.postgresql.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 import java.util.UUID;
 
+
 @Entity
-@Table(name = "tb_terms")
+@Table(
+    name = "tb_terms",
+    indexes = {
+        @Index(name = "idx_terms_term_code", columnList = "term_code"),
+        @Index(name = "idx_terms_start_end", columnList = "start_at,end_at")
+    }
+)
 @Getter
 @NoArgsConstructor
 public class TermsCatalogJpaEntity {
--- src/main/resources/db/migration/V2__add_terms_post_processing_and_template_id.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE tb_terms
-    ADD COLUMN requires_post_processing BOOLEAN NOT NULL DEFAULT FALSE,
-    ADD COLUMN template_id VARCHAR(255);
