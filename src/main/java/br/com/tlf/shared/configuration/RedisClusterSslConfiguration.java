3e9472d80060e55d7e0b6c7f217cb3ac9612f29f
Luiz Lima <luiz.lima@telefonica.com>
2026-09-23 08:39:37 -0300

    bug correção

---
+++ src/main/java/br/com/tlf/shared/configuration/RedisClusterSslConfiguration.java
@@ -0,0 +1,39 @@
+package br.com.tlf.shared.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.data.redis.autoconfigure.ClientResourcesBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.lettuce.core.internal.HostAndPort;
+import io.lettuce.core.resource.DnsResolvers;
+import io.lettuce.core.resource.MappingSocketAddressResolver;
+
+@Configuration
+@ConditionalOnProperty(name = "spring.data.redis.cluster.nodes")
+public class RedisClusterSslConfiguration {
+
+    private final String redisHost;
+
+    public RedisClusterSslConfiguration(@Value("${REDIS_HOST:}") String redisHost) {
+        this.redisHost = redisHost;
+    }
+
+    @Bean
+    public ClientResourcesBuilderCustomizer azureRedisSocketAddressResolver() {
+        return builder -> builder.socketAddressResolver(MappingSocketAddressResolver.create(
+                DnsResolvers.JVM_DEFAULT,
+                endpoint -> cacheIp(redisHost).equals(endpoint.getHostText())
+                        ? HostAndPort.of(redisHost, endpoint.getPort())
+                        : endpoint));
+    }
+
+    private String cacheIp(String host) {
+        try {
+            return DnsResolvers.JVM_DEFAULT.resolve(host)[0].getHostAddress();
+        } catch (Exception e) {
+            return host;
+        }
+    }
+}
